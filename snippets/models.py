2cdea9d3b47bfaaaaa705b797307567bb4112ab2
alinivan-solirius <alin.ivan@solirius.com>
2022-10-19 15:25:57 +0100

    SNIP-19 Added database models and sample data

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Language(models.Model):
+    title = models.CharField(max_length=50)
+
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=100)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    description = models.CharField(max_length=1000)

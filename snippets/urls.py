3a143f3ddd5077c3cf39ee41d7df1ea108e9ddb8
alinivan-solirius <alin.ivan@solirius.com>
2022-10-24 20:30:06 +0100

    SNIP-7 Added search by title terms

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from snippets.views import home, add_snippet, edit_snippet, delete_snippet
+from snippets.views import home, add_snippet, edit_snippet, delete_snippet, search
 
 app_name = 'snippets'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('add_snippet', add_snippet, name='add_snippet'),
     path('edit_snippet/<int:snippet_id>/', edit_snippet, name='edit_snippet'),
-    path('delete_snippet/<int:snippet_id>/', delete_snippet, name='delete_snippet')
+    path('delete_snippet/<int:snippet_id>/', delete_snippet, name='delete_snippet'),
+    path('^$', search, name='search'),
 ]

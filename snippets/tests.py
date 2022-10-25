d3e65840f8a3630b1dab7d300427c8b597f9e3b3
alinivan-solirius <alin.ivan@solirius.com>
2022-10-25 13:38:15 +0100

    SNIP-28 Added read test

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from snippets.models import Snippet, Language
+from django.test.client import RequestFactory
+from snippets.views import home
 
-# Create your tests here.
+
+class SnippetTestCase(TestCase):
+    def setUp(self):
+        Snippet.objects.create(
+            title='Django block of code',
+            language=Language.objects.create(title='Python'),
+            description="def python_function(): return None"
+        )
+        Snippet.objects.create(
+            title='React block of code',
+            language=Language.objects.create(title='JavaScript'),
+            description="function reactComponent(props): return <><h1>Hello {props.name}</h1><>"
+        )
+        self.factory = RequestFactory()
+
+    def test_snippets_display_on_home_page(self):
+        req = self.factory.get("")
+        res = home(req)
+        assert res.status_code == 200
+        assert b"Django block of code" in res.content
+        assert b"React block of code" in res.content

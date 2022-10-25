ebf2b159f2c537c79506ca3e768cc1bb67beb9ab
alinivan-solirius <alin.ivan@solirius.com>
2022-10-25 14:21:12 +0100

    SNIP-29 Added create snippet test

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from snippets.models import Snippet, Language
 from django.test.client import RequestFactory
-from snippets.views import home
+from snippets.views import home, add_snippet
 
 
 class SnippetTestCase(TestCase):
@@ -18,9 +18,23 @@ class SnippetTestCase(TestCase):
         )
         self.factory = RequestFactory()
 
-    def test_snippets_display_on_home_page(self):
+    def test_snippets_displayed_on_home_page(self):
         req = self.factory.get("")
         res = home(req)
         assert res.status_code == 200
         assert b"Django block of code" in res.content
         assert b"React block of code" in res.content
+
+    def test_snippets_created_using_form(self):
+        snippet_object = {
+            'title': 'Another Django block of code',
+            'language': 1,
+            'description': 'def python_function(): return None'
+        }
+        add_req = self.factory.post("add_snippet", snippet_object)
+        print(add_snippet(add_req))
+        home_res = home(self.factory.get(""))
+        assert home_res.status_code == 200
+        assert b"Another Django block of code" in home_res.content
+
+    

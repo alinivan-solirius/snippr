1f5822cdaca949e78fb923dd3e5bdab42457494f
alinivan-solirius <alin.ivan@solirius.com>
2022-10-26 09:23:21 +0100

    SNIP-30 Added update test

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from snippets.models import Snippet, Language
 from django.test.client import RequestFactory
-from snippets.views import home, add_snippet
+from snippets.views import home, add_snippet, edit_snippet
 
 
 class SnippetTestCase(TestCase):
@@ -16,6 +16,12 @@ class SnippetTestCase(TestCase):
             language=Language.objects.create(title='JavaScript'),
             description="function reactComponent(props): return <><h1>Hello {props.name}</h1><>"
         )
+        self.snippet_object = {
+            'title': 'Another Django block of code',
+            'language': 1,
+            'description': 'def python_function(): return None'
+        }
+        self.client = Client(enforce_csrf_checks=True)
         self.factory = RequestFactory()
 
     def test_snippets_displayed_on_home_page(self):
@@ -26,15 +32,19 @@ class SnippetTestCase(TestCase):
         assert b"React block of code" in res.content
 
     def test_snippets_created_using_form(self):
-        snippet_object = {
-            'title': 'Another Django block of code',
-            'language': 1,
-            'description': 'def python_function(): return None'
-        }
-        add_req = self.factory.post("add_snippet", snippet_object)
-        print(add_snippet(add_req))
+        req = self.factory.get("add_snippet")
+        res = add_snippet(req)
+        assert res.status_code == 200
+        self.factory.post("add_snippet", self.snippet_object)
+        home_res = self.client.get("", follow=True)
+        assert home_res.status_code == 200
+        assert b"Django block of code" in home_res.content
+
+    def test_snippets_edited_using_form(self):
+        snippet_id = 1
+        req = self.factory.get("edit_snippet/" + str(snippet_id) + "/")
+        res = edit_snippet(req)
+        self.factory.post("edit_snippet", self.snippet_object)
         home_res = home(self.factory.get(""))
         assert home_res.status_code == 200
-        assert b"Another Django block of code" in home_res.content
-
-    
+        assert b"Django block of code" in home_res.content
